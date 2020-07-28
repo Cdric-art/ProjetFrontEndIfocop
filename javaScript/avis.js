3f069c969035ac725931a8a675de343f55ad7dd6
Evrard Cedric <cdric.art@gmail.com>
2020-07-28 23:48:43 +0200

    feat
    traitement form avis ok
    feat page liens divers

---
+++ javaScript/avis.js
@@ -0,0 +1,15 @@
+document.getElementById('btnContact').addEventListener('click', function (e) {
+    let prenomVal = document.getElementById('prenom').value;
+    let etoileVal = document.getElementById('inputSelect').value;
+    let avisVal = document.getElementById('inputSelectCategorie').value;
+    let msgVal = document.getElementById('message').value;
+    if (prenomVal == undefined || prenomVal.length <= 1) {
+        e.preventDefault();
+    } else if (etoileVal == "Nombre d'étoiles *") {
+        e.preventDefault();
+    } else if (avisVal == "Avis général *") {
+        e.preventDefault();
+    } else if (msgVal == undefined || msgVal.length <= 1) {
+        e.preventDefault();
+    };
+});
