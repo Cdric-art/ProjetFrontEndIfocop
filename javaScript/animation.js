d9164659eebf211664968a61a7d3d117bfed74c0
Evrard Cedric <cdric.art@gmail.com>
2020-08-01 15:37:06 +0200

    LetterbyLetter
    Animation JS  index.html H1

---
+++ javaScript/animation.js
@@ -0,0 +1,26 @@
+/* *** ANIMATION LETTRE PAR LETTRE TITRE H1 INDEX.HTML *** */
+
+/* ELEMENT H1 */
+const element = document.getElementById('hello'); 
+
+/* FUNCTION QUI SUPPRIME LE CONTENU HTML DU H1 */
+function Title () {
+    element.innerHTML = '';
+};
+
+/* FUNCTION QUI RECUPERE LA VALEUR DE L'ATTRIBUT NAME DU H1 */
+function LetterByLetter () {
+    let i = 0;
+    let message = element.getAttribute('name');
+    const interval = setInterval (() => {
+        element.innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length){
+            clearInterval(interval);
+        }
+    }, 250);
+};
+
+/* EXECUTION DES FONCTIONS */
+setTimeout(Title, 2000);
+setTimeout(LetterByLetter, 2500);
