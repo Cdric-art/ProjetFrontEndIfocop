8d43d46dbd6b81f98bf76edc8cf21d496a3d53c6
Evrard Cedric <cdric.art@gmail.com>
2020-08-01 14:52:16 +0200

    feat

---
+++ javaScript/panier.js
@@ -0,0 +1,8 @@
+const check = document.getElementById('checkbox');
+
+document.getElementById('btnPanier').addEventListener('click', function (e) {
+    var checkVal = check.checked;
+    if (checkVal === false) {
+        e.preventDefault();
+    }
+});
