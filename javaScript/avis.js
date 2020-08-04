fd28cc0240b8c1ece8ac0157fa79e1aee20778c8
Evrard Cedric <cdric.art@gmail.com>
2020-08-04 18:28:37 +0200

    fix
    fix: article blog
    fix buton outline chrome
    feat: ajout de commentaire

--- javaScript/avis.js
+++ javaScript/avis.js
@@ -1,17 +1,43 @@
 /* *** TRAITEMENT FORMULAIRE *** */
 
 document.getElementById('btnContact').addEventListener('click', function (e) {
+    //e.preventDefault(); // A dé-commenter pour tester l'ajout de commentaire !
+
     let prenomVal = document.getElementById('prenom').value;
     let etoileVal = document.getElementById('inputSelect').value;
-    let avisVal = document.getElementById('inputSelectCategorie').value;
     let msgVal = document.getElementById('message').value;
     if (prenomVal == undefined || prenomVal.length <= 1) {
         e.preventDefault();
     } else if (etoileVal == "Nombre d'étoiles *") {
         e.preventDefault();
-    } else if (avisVal == "Avis général *") {
-        e.preventDefault();
     } else if (msgVal == undefined || msgVal.length <= 1) {
         e.preventDefault();
     };
+
+
+    /* *** AJOUTER UN COMMENTAIRE DANS GENERAL *** */
+    /*
+    const divSpeudo = document.getElementById('speudo');
+    const paragraphSpeudo = document.createElement('p');
+    paragraphSpeudo.innerHTML = prenomVal;
+    divSpeudo.appendChild(paragraphSpeudo);
+
+    const divEtoile = document.getElementById('etoile');
+    const paragraphEtoile = document.createElement('p');
+    if (etoileVal == 'Cinq étoiles') {
+        paragraphEtoile.innerHTML = '*****';
+    } else if (etoileVal == 'Quatre étoiles') {
+        paragraphEtoile.innerHTML = '****';
+    } else if (etoileVal == 'Trois étoiles') {
+        paragraphEtoile.innerHTML = '***';
+    } else if (etoileVal == 'Deux étoiles') {
+        paragraphEtoile.innerHTML = '**';
+    } else {
+        paragraphEtoile.innerHTML = '*';
+    };
+    divEtoile.appendChild(paragraphEtoile);
+    const divCommentaire = document.getElementById('commentaire');
+    const paragraphCommentaire = document.createElement('p');
+    paragraphCommentaire.innerHTML = msgVal;
+    divCommentaire.appendChild(paragraphCommentaire); */
 });
