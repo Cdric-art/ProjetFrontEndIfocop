df810142d2e275813b2569a7a36e459b6d001cae
Evrard Cedric <cdric.art@gmail.com>
2020-08-05 18:14:19 +0200

    js
    feat avis.js -> Dépot de commentaire

--- javaScript/avis.js
+++ javaScript/avis.js
@@ -4,26 +4,38 @@
 document.getElementById('btnContact').addEventListener('click', function (e) {
     e.preventDefault();
 
+    // Récupération des valeurs de chaques input
     let prenomVal = document.getElementById('prenom').value;
     let etoileVal = document.getElementById('inputSelect').value;
     let avisVal = document.getElementById('inputSelectCategorie').value;
     let msgVal = document.getElementById('message').value;
-    console.log(etoileVal);
-    // if (prenomVal == undefined || prenomVal.length <= 1) {
-    //     e.preventDefault();
-    // } else if (etoileVal == "Nombre d'étoiles *") {
-    //     e.preventDefault();
-    // } else if (msgVal == undefined || msgVal.length <= 1) {
-    //     e.preventDefault();
-    // };
+    
+    // Sélection des divs dans lesquelles les paragraphes seront ajouté
+    // Div pour Avis Généraux 
+    const divSpeudo = document.getElementById('divSpeudo');
+    const divEtoile = document.getElementById('divEtoile');
+    const divCommentaire = document.getElementById('divCommentaire');
+    // Div pour Avis Chambres
+    const divSpeudoChambre = document.getElementById('divChambreSpeudo');
+    const divEtoileChambre = document.getElementById('divChambreEtoile');
+    const divCommentaireChambre = document.getElementById('divChambreCommentaire');
+    // Div pour Avis Restaurant
+    const divSpeudoRestaurant = document.getElementById('divRestaurantSpeudo');
+    const divEtoileRestaurant = document.getElementById('divRestaurantEtoile');
+    const divCommentaireRestaurant = document.getElementById('divRestaurantCommentaire');
+    //Div pour Avis Spa
+    const divSpeudoSpa = document.getElementById('divSpaSpeudo');
+    const divEtoileSpa = document.getElementById('divSpaEtoile');
+    const divCommentaireSpa = document.getElementById('divSpaCommentaire');
 
-    const divSpeudo = document.getElementById('speudo');
+    // Création des paragraphes
     const paragraphSpeudo = document.createElement('p');
-    paragraphSpeudo.innerHTML = prenomVal;
-    divSpeudo.appendChild(paragraphSpeudo);
-
-    const divEtoile = document.getElementById('etoile');
     const paragraphEtoile = document.createElement('p');
+    const paragraphCommentaire = document.createElement('p');
+
+    // Ajout des valeurs aux paragraphes
+    paragraphSpeudo.innerHTML = prenomVal;
+    
     if (etoileVal == 'Cinq étoiles') {
         paragraphEtoile.innerHTML = '*****';
     } else if (etoileVal == 'Quatre étoiles') {
@@ -35,9 +47,25 @@ document.getElementById('btnContact').addEventListener('click', function (e) {
     } else {
         paragraphEtoile.innerHTML = '*';
     };
-    divEtoile.appendChild(paragraphEtoile);
-    const divCommentaire = document.getElementById('commentaire');
-    const paragraphCommentaire = document.createElement('p');
+
     paragraphCommentaire.innerHTML = msgVal;
-    divCommentaire.appendChild(paragraphCommentaire); 
+
+    // Insert des parapgraphes dans les div correspondantes à la catégorie sélectionnée
+    if (avisVal == 'Avis général *') {
+        divSpeudo.appendChild(paragraphSpeudo);
+        divEtoile.appendChild(paragraphEtoile);
+        divCommentaire.appendChild(paragraphCommentaire);
+    } else if (avisVal == 'Avis chambre') {
+        divSpeudoChambre.appendChild(paragraphSpeudo);
+        divEtoileChambre.appendChild(paragraphEtoile);
+        divCommentaireChambre.appendChild(paragraphCommentaire);
+    } else if (avisVal == 'Avis restaurant') {
+        divSpeudoRestaurant.appendChild(paragraphSpeudo);
+        divEtoileRestaurant.appendChild(paragraphEtoile);
+        divCommentaireRestaurant.appendChild(paragraphCommentaire);
+    } else {
+        divSpeudoSpa.appendChild(paragraphSpeudo);
+        divEtoileSpa.appendChild(paragraphEtoile);
+        divCommentaireSpa.appendChild(paragraphCommentaire);
+    };     
 });
